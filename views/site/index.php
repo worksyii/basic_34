bab1fa033a12280a9d65de67d711c183fba889a4
Slava <veacesalv.bodistean@gmail.com>
2020-04-29 01:38:50 +0300

    edit index fot homecart index

--- views/site/index.php
+++ views/site/index.php
@@ -1,17 +1,53 @@
-<?php
+<!DOCTYPE html>
+<!-- saved from url=(0044)http://demo.whatacart.com/site/default/index -->
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
-/* @var $this yii\web\View */
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="language" content="en-US">
+    <link rel="icon" type="image/ico" href="http://demo.whatacart.com/themes/classic/assets/images/favicon.ico">
+    <title>Home</title>
+    <meta name="csrf-param" content="_frontendCSRF">
+    <meta name="csrf-token" content="ckNmYnFHYmo1FVRXBQFbLx1xXzsECRc/KC0NBxYVIFwDDC1bPwshHA==">
+    <meta name="description" content="Demo site Description">
+    <meta name="keywords" content="Demo site Keywords">
+    <link href="./Home_files/bootstrap.min.css" rel="stylesheet">
+    <link href="./Home_files/font-awesome.min.css" rel="stylesheet">
+    <link href="./Home_files/stylesheet.css" rel="stylesheet">
+    <link href="./Home_files/bootstrap-theme.css" rel="stylesheet">
+    <link href="./Home_files/css" rel="stylesheet">    </head>
 
-$this->title = 'Techno Restart';
+<body>
+<nav id="top">
+    <div class="container">
+        <div class="hidden-xs hidden-sm hidden-md pull-left" id="local-options">
+            <ul class="list-inline">
+                <li class="dropdown"><a class="dropdown-toggle" href="http://demo.whatacart.com/site/default/index#" data-toggle="dropdown"><span>USD</span>
+                        <i class="fa fa-caret-down"></i></a><ul id="w0" class="dropdown-menu"><li><a class="currency-selector" href="http://demo.whatacart.com/site/default/index#" data-currency-id="USD" tabindex="-1">USD</a></li>
+                        <li><a class="currency-selector" href="http://demo.whatacart.com/site/default/index#" data-currency-id="GBP" tabindex="-1">GBP</a></li></ul></li><li class="dropdown"><a class="dropdown-toggle" href="http://demo.whatacart.com/site/default/index#" data-toggle="dropdown"><i class="fa fa-shopping-basket"></i>
+                        <span>Default</span>
+                        <i class="fa fa-caret-down"></i></a><ul id="w1" class="dropdown-menu"><li><a class="store-selector" href="http://demo.whatacart.com/site/default/index#" data-store-id="1" tabindex="-1">Default</a></li>
+                        <li><a class="store-selector" href="http://demo.whatacart.com/site/default/index#" data-store-id="2" tabindex="-1">Demo</a></li></ul></li>            </ul>
+        </div>
+        <div id="top-links" class="nav pull-right">
+            <ul class="list-inline">
+                <li> <strong>Demo will be reset in <span id="w2">02:31:29</span></strong> </li>
+                <li class="dropdown">
+                    <a href="http://demo.whatacart.com/customer/site/login" title="My Account" class="dropdown-toggle" data-toggle="dropdown">
+                        <span class="hidden-xs hidden-sm hidden-md">My Account</span>
+
+$this->title = 'TR MDL';
 ?>
 <div class="site-index">
 
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
+                <li>
+                    <a href="http://demo.whatacart.com/wishlist/default/view" id="wishlist-total" title="Wish List" class="top-nav-wishlist">
+                        <span class="hidden-xs hidden-sm hidden-md">Wish List</span>
+                    </a>
+                </li>
 
-        <p class="lead">You have successfully on Techno Restart</p>
+        <p class="lead">You have successfully on TR MDL</p>
 
-        <p><a class="btn btn-lg btn-success" href="#">Click here!</a></p>
+        <p><a class="btn btn-lg btn-success" href="#">OK</a></p>
     </div>
 
 
