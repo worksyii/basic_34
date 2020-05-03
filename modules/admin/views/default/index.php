da79eb87341e342ffc6d34a6e477e005ce3da462
Gherasimov Alexandru <gherasimovalexandru@gmail.com>
2020-05-03 19:16:28 +0300

    deleted data

--- modules/admin/views/default/index.php
+++ modules/admin/views/default/index.php
@@ -2,17 +2,17 @@
 
 /* @var $this yii\web\View */
 
-$this->title = 'Tehno Restart';
+$this->title = 'Techno Restart';
 ?>
 
 <div class="admin-default-index">
 
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
+<div class="jumbotron">
+    <h1>Congratulations!</h1>
 
-        <p class="lead">You have successfully on Tehno Restart</p>
+    <p class="lead">You have successfully on Tehno Restart</p>
 
-        <p><a class="btn btn-lg btn-success" href="#">OK</a></p>
-    </div>
+    <p><a class="btn btn-lg btn-success" href="#">OK</a></p>
+</div>
 
 </div>
